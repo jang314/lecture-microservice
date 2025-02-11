93e9ef8f2e303faa059c1362df0016c5e65a9b77
jang314 <jang314@naver.com>
2025-02-11 14:23:41 +0900

    API Gateway 수강 완료

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,6 +1,9 @@
 package com.example.secondservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/second-service")
 public class SecondServiceController {
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -20,8 +25,10 @@ public class SecondServiceController {
         log.info(header);
         return "Hello World in Second Service";
     }
+
     @GetMapping("/check")
-    public String check() {
-        return "Hi, There. This is a message from Second Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, There. This is a message from Frist Service on Port %s", env.getProperty("local.server.port"));
     }
 }
